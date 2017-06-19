8aa7e1eab0a224d590f7cd55521d68e89fa272e2
walker <875504663@qq.com>
2017-06-19 22:16:12 +0800

    longxie
    累死了

--- HospitalBible/HospitalBible/Home/ViewController/SelfTestViewController.m
+++ HospitalBible/HospitalBible/Home/ViewController/SelfTestViewController.m
@@ -15,15 +15,16 @@
 #import "UIButton+ImageTitleSpacing.h"
 #import "UIView+ActivityIndicator.h"
 #import "HomeViewModel.h"
-@interface SelfTestViewController ()<UICollectionViewDelegate, UICollectionViewDataSource,UIScrollViewDelegate>
-@property (nonatomic, strong) UICollectionView *collectionView;
-@property(nonatomic,strong) NSMutableArray *dataSources;
+#import "SwipeView.h"
+#import "LGQuestionView.h"
+
+@interface SelfTestViewController ()<SwipeViewDataSource, SwipeViewDelegate>
 @property(nonatomic,assign) NSInteger time;
 @property(nonatomic,strong) UILabel *timeLabel;
 @property(nonatomic,strong) UIImageView *clockImageView;
-@property(nonatomic,assign) CGFloat scrollIndex;
-@property(nonatomic,strong) UIButton *summitButton;
 @property(nonatomic,strong) NSTimer *timer;
+
+@property(nonatomic,strong) SwipeView *swipeView;
 @end
 
 @implementation SelfTestViewController
@@ -32,43 +33,58 @@
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
     self.view.backgroundColor = [UIColor whiteColor];
-    //[self test];
+    self.title = @"自检答题";
     
     //1.顶部倒计时
     [self.view addSubview:self.clockImageView];
     [self.view addSubview:self.timeLabel];
     
-    //2.题目
-    [self.view addSubview:self.collectionView];
-    
-    //3.最后一个单元格，添加提交按钮
-    [self initSummitButton];
-    
     self.timer = [NSTimer scheduledTimerWithTimeInterval:1 repeats:YES block:^(NSTimer * _Nonnull timer) {
         _time++;
         _timeLabel.text = [NSString stringWithFormat:@"倒计时:%@", getMMSSFromSS(_time)];
-        
     }];
     
-//    [HomeViewModel requestDiseasequestionListWithClassId:@"2" successHandler:^(id result) {
-//        self.dataSources = result;
-//        [self.collectionView reloadData];
-//    } errorHandler:^(NSError *error) {
-//        
-//    }];
-
-}
-- (NSInteger)collectionView:(UICollectionView *)collectionView numberOfItemsInSection:(NSInteger)section
-{
-    return self.dataSources.count;
+    self.swipeView = [[SwipeView alloc] initWithFrame:CGRectMake(0, 160, SCREEN_WIDTH, SCREEN_HEIGHT-160)];
+    self.swipeView.dataSource = self;
+    self.swipeView.delegate = self;
+    [self.view addSubview:self.swipeView];
 }
 
-- (UICollectionViewCell *)collectionView:(UICollectionView *)collectionView cellForItemAtIndexPath:(NSIndexPath *)indexPath
+- (NSInteger)numberOfItemsInSwipeView:(SwipeView *)swipeView
 {
-    SelfTestQuestionsCell *cell = [collectionView dequeueReusableCellWithReuseIdentifier:@"SelfTestQuestionsCell" forIndexPath:indexPath];
-    cell.mdel = self.dataSources[indexPath.row];
-    return cell;
+    return 3;
 }
+- (UIView *)swipeView:(SwipeView *)swipeView viewForItemAtIndex:(NSInteger)index reusingView:(UIView *)view
+{
+    LGQuestionView *scrollView = (LGQuestionView *)view;
+    if (scrollView == nil)
+    {
+       
+        scrollView = [[LGQuestionView alloc] initWithFrame:self.swipeView.bounds];
+        view = scrollView;
+    }
+    
+    
+    scrollView.dataArray = @[@"1",@"2",@"3"];
+
+    
+//    CGFloat red = arc4random() / (CGFloat)INT_MAX;
+//    CGFloat green = arc4random() / (CGFloat)INT_MAX;
+//    CGFloat blue = arc4random() / (CGFloat)INT_MAX;
+//    view.backgroundColor = [UIColor colorWithRed:red
+//                                           green:green
+//                                            blue:blue
+//                                           alpha:1.0];
+    view.backgroundColor = [UIColor whiteColor];
+    
+    return view;
+};
+
+- (CGSize)swipeViewItemSize:(SwipeView *)swipeView
+{
+    return self.swipeView.bounds.size;
+}
+
 -(void)test{
     
     AnswerModel *model1 = [AnswerModel new];
@@ -95,35 +111,9 @@
     model3.result =@[@"A",@"C"];
     model3.selectedResult = @"";
     
-    [self.dataSources addObjectsFromArray:@[model1,model2,model3]];
+//    [self.dataSources addObjectsFromArray:@[model1,model2,model3]];
 }
 
--(NSMutableArray *)dataSources{
-    if (!_dataSources) {
-        _dataSources = [NSMutableArray array];
-    }
-    return _dataSources;
-}
-
--(UICollectionView *)collectionView{
-    if (!_collectionView) {
-        UICollectionViewFlowLayout *flowLayout = [[UICollectionViewFlowLayout alloc] init];
-        
-        flowLayout.itemSize = CGSizeMake(SCREEN_WIDTH, SCREEN_HEIGHT-160);
-        [flowLayout setScrollDirection:UICollectionViewScrollDirectionHorizontal];
-        flowLayout.sectionInset = UIEdgeInsetsMake(0, 0, 0, 0);
-        flowLayout.minimumLineSpacing = 0;
-        
-        _collectionView = [[UICollectionView alloc] initWithFrame:CGRectMake(0, CGRectGetMaxY(self.timeLabel.frame)+10, SCREEN_WIDTH, SCREEN_HEIGHT-160) collectionViewLayout:flowLayout];
-        _collectionView.backgroundColor = [UIColor whiteColor];
-        _collectionView.bounces = NO;
-        _collectionView.delegate = self;
-        _collectionView.dataSource = self;
-        _collectionView.pagingEnabled  = YES;
-        [_collectionView registerNib:[UINib nibWithNibName:@"SelfTestQuestionsCell" bundle:nil] forCellWithReuseIdentifier:@"SelfTestQuestionsCell"];
-    }
-    return _collectionView;
-}
 -(UIImageView *)clockImageView{
     if (!_clockImageView) {
         _clockImageView = [[UIImageView alloc] initWithFrame:CGRectMake(0, 0, 139, 46)];
@@ -144,42 +134,5 @@
     }
     return _timeLabel;
 }
--(void)initSummitButton{
-    UIButton *summit = [UIButton buttonWithType:(UIButtonTypeCustom)];
-    summit.backgroundColor = [UIColor redColor];
-    [summit setTitle:@"提交" forState:(UIControlStateNormal)];
-    summit.frame = CGRectMake(0, 0, SCREEN_WIDTH*0.5, 44);
-    summit.center = CGPointMake((self.dataSources.count-1)*SCREEN_WIDTH+SCREEN_WIDTH*0.5, self.collectionView.frame.size.height - 22-10);
-    summit.layer.cornerRadius = 22;
-    summit.backgroundColor = [UIColor colorWithHexString:@"0FA49E"];
-    summit.layer.masksToBounds = YES;
-    [self.collectionView addSubview:summit];
-    [summit bk_addEventHandler:^(id  _Nonnull sender) {
-        
-        if ([self isNoCompleteQuestions]) {
-            [self showErrorMessage:@"您还有未打完的题目"];
-        }else{//提交,上送结果到服务器
-            [_timer invalidate];
-            _time = 0;
-            [self showErrorMessage:@"提交成功"];
-        }
-    } forControlEvents:(UIControlEventTouchUpInside)];
-}
 
--(BOOL)isNoCompleteQuestions{
-    for (AnswerModel *model in self.dataSources) {
-        if ([model.selectedResult isEqualToString:@""]) {
-            return YES;
-        }
-    }
-    return NO;
-}
--(NSArray*)getCompletedAnswersResult{
-    NSMutableArray *results = [NSMutableArray array];
-    for (AnswerModel *model in self.dataSources) {
-        [results addObject:model.selectedResult];
-    }
-    return results;
-    
-}
 @end
